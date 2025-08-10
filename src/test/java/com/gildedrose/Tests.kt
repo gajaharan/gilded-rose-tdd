2ddcc2c20804176697057fc13e64e246b426483c
gajaharan <gajaharan@gmail.com>
2025-08-10 20:05:55 +0100

    print stock list

--- src/test/java/com/gildedrose/Tests.kt
+++ src/test/java/com/gildedrose/Tests.kt
@@ -6,7 +6,7 @@ import kotlin.test.assertEquals
 
 class Tests {
     @Test
-    fun test() {
+    fun `Add item to stock`() {
         val stock = listOf<Item>()
 
         assertEquals(listOf<Item>(),
@@ -18,5 +18,31 @@ class Tests {
             newStock)
 
     }
+
+    @Test
+    fun `print stock list`() {
+        val stock = listOf<Item>()
+        val expected = listOf("10 August 2025")
+
+        assertEquals(expected, stock.printout())
+    }
+
+    @Test
+    fun `print non empty stock list`() {
+       val stock = listOf(
+           Item("banana", LocalDate.now(), 42),
+           Item("kumquat", LocalDate.now(), 101)
+       )
+
+        val expected = listOf(
+            "10 August 2025"
+        )
+
+        assertEquals(expected, stock.printout())
+    }
 }
+ private fun List<Item>.printout(): List<String> {
+     return listOf("10 August 2025")
+ }
+
 
