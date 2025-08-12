8f1c6a0eb67fdfb620c43d39c4ffc5a17b75fe5a
gajaharan <gajaharan@gmail.com>
2025-08-12 20:28:37 +0100

    Return back name, sellByDate, sellByDays and quantity in the html response

--- src/test/java/com/gildedrose/ListStockTests.kt
+++ src/test/java/com/gildedrose/ListStockTests.kt
@@ -24,32 +24,72 @@ class ListStockTests {
             Item("kumquat", now.plusDays(1), 101)
         )
 
-        val server: Server = Server(stock)
+        val server: Server = Server(stock) { now }
         val client: RoutingHttpHandler = server.routes
         val response: Response = client(Request(Method.GET, "/"))
-        assertEquals(templateSource, response.bodyString())
+        assertEquals(expected, response.bodyString())
     }
 }
 
 @Language("HTML")
-val templateSource = """
+val expected = """
     <html>
     <body>
-    <tr></tr>
+    <tr>
+        <td>banana</td></tr>
+        <td>9 August 2025</td>
+        <td>-1</td>
+        <td>42</td>
+    </tr>
+    <tr>
+        <td>kumquat</td></tr>
+        <td>11 August 2025</td>
+        <td>1</td>
+        <td>101</td>
+    </tr>
+
     </body>
     </html>
     """.trimIndent()
 
 class Client(server: Server)
 
-class Server(stock: List<Item>) {
+class Server(
+    stock: List<Item>,
+    clock: () -> LocalDate = { LocalDate.now() }
+) {
     val handlebars = Handlebars()
     val rootTemplate = handlebars.compile(
         StringTemplateSource("no such file", templateSource)
     )
     val routes = routes(
         "/" bind Method.GET to { request ->
-            Response(Status.OK).body(rootTemplate.apply(stock))
+            val now = clock()
+            Response(Status.OK).body(rootTemplate
+                .apply(stock.map {
+                    it.toMap(now)
+                }))
         }
     )
 }
+
+private fun Item.toMap(now: LocalDate): Map<String,String> = mapOf(
+    "name" to name,
+    "sellByDate" to format.format(sellByDate),
+    "sellByDays" to this.daysUntilSellBy(now).toString(),
+    "quantity" to quantity.toString()
+)
+@Language("HTML")
+val templateSource = """
+    <html>
+    <body>
+    {{#each}}<tr>
+        <td>{{this.name}}</td></tr>
+        <td>{{this.sellByDate}}</td>
+        <td>{{this.sellByDays}}</td>
+        <td>{{this.quantity}}</td>
+    </tr>
+    {{/each}}
+    </body>
+    </html>
+    """.trimIndent()
