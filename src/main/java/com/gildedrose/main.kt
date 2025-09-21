d67fe323cdf3872d14d930d2955853d5020ee3ed
gajaharan <gajaharan@gmail.com>
2025-09-21 18:49:40 +0100

    Bring more of main under test

--- src/main/java/com/gildedrose/main.kt
+++ src/main/java/com/gildedrose/main.kt
@@ -5,7 +5,7 @@ import java.io.File
 import java.time.LocalDate
 
 fun main() {
-    val file = File("stock.tsv").also { it.createNewFile() }
+    val file = File("stock.tsv")
     val routes = routesFor(file)
     val server = Server(routes)
     server.start()
@@ -16,6 +16,5 @@ fun routesFor(
     clock: () -> LocalDate = LocalDate::now
 ): RoutingHttpHandler {
     val stock = file.loadItems()
-    stock.saveTo(file)
-    return routes(stock = stock)
+    return routes(stock = stock, clock)
 }
